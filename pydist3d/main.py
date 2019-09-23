dd274771643dbd54dd8d164987957b20b03d980b
Jeremy Metz <metz.j@gmail.com>
2019-09-23 14:28:21 +0100

    Continued refactoring

--- pydist3d/main.py
+++ pydist3d/main.py
@@ -1,28 +1,28 @@
+"""main.py
+
+Main entry module for pydist3d
+
+J. Metz <metz.jp@gmail.com>
+"""
 import os
 import csv
-import logging
-import coloredlogs
 import numpy as np
 import tifffile
 import matplotlib.pyplot as plt
-import scipy.ndimage as ndi
 import skimage.filters as skfilt
-
-
-logger = logging.getLogger(__name__)
-# Default format for coloredlogs is
-# "%(asctime)s %(hostname)s %(name)s[%(process)d] %(levelname)s %(message)s"
-coloredlogs.install(
-    fmt="%(asctime)s %(name)s:%(lineno)d %(levelname)s %(message)s",
-    level='DEBUG', logger=logger
-)
+from pydist3d import distance_analysis
+from pydist3d.utility import logger
 
 
 # ------------------------------
 # Add in calibration of pixels in xy and z directions
 # ------------------------------
-zmicsperpix = 0.75
-xymicsperpix = 0.08
+__zmicsperpix__ = 0.75
+__xymicsperpix__ = 0.08
+
+distance_analysers = {
+    'edge-edge': distance_analysis.analyse_distances_edge_edge,
+}
 
 
 def get_files(folder):
@@ -34,25 +34,12 @@ def get_files(folder):
     return filename_list
 
 
-def batch(input_folder, output_folder):
-    """
-    Batch process all the data files in a given folder,
-    saving results to the nomincated `output_folder`.
-    """
-    filename_list = get_files(input_folder)
-
-    for index, filepath in enumerate(filename_list):
-        filename = os.path.basename(filepath)
-        logger.info(
-            "processing file{}, this is file {} of {}".format(
-                filename, index + 1, len(filename_list)))
-        process_file(filepath, output_folder)
-
-
-def process_file(filepath, output_folder):
+def process_file(filepath, output_folder, distance_analyser='edge-edge'):
     """
     Perform main processing on the file
     """
+    if isinstance(distance_analyser, str):
+        distance_analyser = distance_analysers[distance_analyser]
     # load the data using tifffile, unlike when
     # loading .czi files, no unnecessary
     # dimensions are added, meaning that we do not need to
@@ -67,6 +54,10 @@ def process_file(filepath, output_folder):
         raise ValueError(
             "Unhandled dimensionality of data"
             + f"\nData must be 3-4 dimensional, got {data.ndim}")
+    elif data.ndim < 3:
+        raise ValueError(
+            "Input image must contain 2 channels and therefore"
+            " be at least 3d")
 
     # split channels
     channel1 = data[0]
@@ -89,20 +80,6 @@ def process_file(filepath, output_folder):
     mask1 = filtered1 >= threshold_value1
     mask2 = filtered2 >= threshold_value2
 
-    # Generate distance map of mask2 - Note: because the distance
-    # map here measures how far to the nearest OFF pixel (0), we need to
-    # invert the mask while performing this function
-    # Aside: the edt part of the function just stands for
-    # Exact euclidean distance transform, as the module
-    # also has other ways of calculating different
-    # distance transforms!
-    # NOTE: Includes pixel sampling factors;
-    # so distances will be in microns
-    sampling = [xymicsperpix for shape in channel1.shape]
-    if len(sampling) == 3:
-        sampling[0] = zmicsperpix
-
-    distancemap2 = ndi.distance_transform_edt(~mask2, sampling=sampling)
 
     # We now create overlay figures for channel 1 and 2
     # and save to the input folder to allow
@@ -121,82 +98,15 @@ def process_file(filepath, output_folder):
     # "Analyze particles" in imagej; it identifies
     # connected clumps of pixels and gives them all a unique
     # label.
+    distance_analyser(
+        mask1, mask2,
+        xymicsperpix=__xymicsperpix__, zmicsperpix=__zmicsperpix__)
 
-    labels1, num_objects1 = ndi.label(mask1)
 
-    if num_objects1 > 500:
-        logger.critical(f"Too many objects found ({num_objects1}), skipping")
-        return
-    # NB: As this function is in scipy.ndimage - it happily handles
-    # N-d data for us!
-    # It also outputs the number of objects found, so we can show that
-    # in the terminal...
-
-    logger.info(f"Number of objects in mask1: {num_objects1}")
-
-    # Next we want to process each labelled region (~object)
-    # individually, so we will use a for-loop
-    # We know how many objects there are from num_objects1
-
-    # NOTE: The Python range function, when called with two inputs
-    # generates the numbers input1, input1+1, input1+2, ... , input2-1
-    # I.e. it does not generate input2, so if we want to have a
-    # range of numbers go to X, then we have to use X+1 as the second input!
-
-    # We're also going to create a list of distances - one
-    # entry per object, but each entry itself is going to
-    # be a list of distances!
-
-    # First, let's create an empty list using []
-    distances_list = []
-    dist_stats_list = [
-        [
-            "min distance",
-            "max distance",
-            "mean distance",
-            "median distance",
-            "sum distance",
-            "total number of bacterial perimiter pixels",
-        ]
-    ]
-
-    for label in range(1, num_objects1 + 1):
-
-        # Now we generate a mask of just this object
-        # which is kind of like an ROI object in imagej
-        # We can do this by using the comparison operator ==
-        # which evaluates to true where values in labels1 are equal
-        # to label
-        mask_obj = labels1 == label
-
-        # Now we can generate an outline of this region by performing
-        # binary erosion of this region and getting the pixels where
-        # mask_obj is True, but the eroded version is False!
-        eroded = ndi.binary_erosion(mask_obj)
-        outline = mask_obj & ~eroded
-
-        # Now we can get all the distances of the outline pixels
-        # to the nearest object in mask2!
-        distances = distancemap2[outline]
-        dist_arr = np.array(distances)
-        dist_stats = [
-            np.min(dist_arr),
-            np.max(dist_arr),
-            np.mean(dist_arr),
-            np.median(dist_arr),
-            np.sum(dist_arr),
-            dist_arr.shape[0],
-        ]
-        # This uses "logical indexing", i.e. we can pass in a mask array
-        # to extract only pixels in distancemap2 where the mask array
-        # (in this case outline) is True.
-
-        # Lastly we just need to decide what to do with the distances...
-        # For now, let's create a table, so we're going to append the
-        # current distances to the end of the list
-        distances_list.append(distances)
-        dist_stats_list.append(dist_stats)
-
+def output_distances_and_stats(output_folder, distances_list, dist_stats_list):
+    """
+    Outputs the distances and stats
+    """
     # Now that we have all the distances in a list, we could
     # perform statistical tests on them, or get their means,
     # maxima etc, but for now let's just write them to a
@@ -266,6 +176,21 @@ def plot_and_save_outlines(channel1, channel2, mask1, mask2, filepath):
     plt.savefig("{}_mask2.png".format(filepath))
     plt.close()
     logger.info("Created overlay images")
+
+
+def batch(input_folder, output_folder):
+    """
+    Batch process all the data files in a given folder,
+    saving results to the nominated `output_folder`.
+    """
+    filename_list = get_files(input_folder)
+
+    for index, filepath in enumerate(filename_list):
+        filename = os.path.basename(filepath)
+        logger.info(
+            "processing file{}, this is file {} of {}".format(
+                filename, index + 1, len(filename_list)))
+        process_file(filepath, output_folder)
 
 
 def main(input_folder, output_folder=None):
