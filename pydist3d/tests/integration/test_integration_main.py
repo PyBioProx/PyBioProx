a889c95a1433f52706391195436e72d4eee946c0
Jeremy Metz <metz.jp@gmail.com>
2019-09-30 15:02:51 +0100

    Reorganised tests, adding integration test

---
+++ pydist3d/tests/integration/test_integration_main.py
@@ -0,0 +1,28 @@
+"""test_integration_main.py
+
+Integration tests
+
+J. Metz <metz.jp@gmail.com>
+"""
+import numpy as np
+import imageio
+import scipy.ndimage as ndi
+
+
+def create_temp_image(tmp_path):
+    """Utility to create temporary data"""
+    image = np.random.rand(2, 100, 100)
+    image = ndi.gaussian_filter(image, sigma=[0, 3, 3])
+    image -= image.min()
+    image /= image.max()
+    image = (image > 0.6).astype('uint8')
+    tempfile = tmp_path / "data.tif"
+    imageio.volwrite(tempfile, image, format='tiff')
+    return tempfile
+
+
+def test_main(tmp_path):
+    """Coarse-grained test that this thing runs properly"""
+    imagefile = create_temp_image(tmp_path)
+    print("Created temporary image at", imagefile)
+
