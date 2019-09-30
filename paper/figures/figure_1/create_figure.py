186c1ba4caf20c6c250446321207c3b5797f3dff
Jeremy Metz <metz.jp@gmail.com>
2019-09-30 15:08:15 +0100

    Added initial plotting script for figure 1

---
+++ paper/figures/figure_1/create_figure.py
@@ -0,0 +1,17 @@
+"""create_figure.py
+
+Creating figure 1
+
+J. Metz <metz.jp@gmail.com>
+"""
+
+
+def main():
+    """
+    Main entry point function
+    """
+    print("Running create_figure.py for figure1")
+
+
+if __name__ == '__main__':
+    main()
