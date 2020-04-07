fb494f9e1e9c581173e2175f595c94701ae1e893
Jeremy Metz <metz.jp@gmail.com>
2020-04-07 16:54:43 +0200

    Added setup.py file - project coordination

---
+++ setup.py
@@ -0,0 +1,74 @@
+#!/usr/bin/env python
+"""setup.py
+
+Setup script for pydist3d module
+
+J. Metz <metz.jp@gmail.com>
+"""
+import os
+import unittest
+from typing import List
+from setuptools import setup, Command
+# Coverage command
+import coverage
+import pytest
+
+MODNAME = "pydist3d"
+
+
+class CoverageCommand(Command):
+    """Coverage Command"""
+    description = "Run coverage on unit-tests (not integration tests!)"
+    user_options: List = []
+
+    def initialize_options(self):
+        """init options"""
+        pass
+
+    def finalize_options(self):
+        """finalize options"""
+        pass
+
+    def run(self):  # pylint: disable=no-self-use
+        """runner"""
+        # NOTE: Use API as follows for better control
+        omitfiles = [
+            os.path.join(MODNAME, "tests", "unit", "*"),
+            os.path.join(MODNAME, "__main__.py"),
+        ]
+        for root, _, files in os.walk(MODNAME):
+            omitfiles.extend(
+                os.path.join(root, fname) for fname in
+                filter(lambda f: f == "__init__.py", files)
+            )
+
+        print("Running coverage on", MODNAME)
+        cov = coverage.Coverage(
+            source=[MODNAME],
+            omit=omitfiles,
+            )
+        cov.start()
+        # Run normal tests
+        # loader = unittest.TestLoader()
+        # tests = loader.discover(MODNAME)
+        # runner = unittest.runner.TextTestRunner()
+        # runner.run(tests)
+        pytest.main([])
+        cov.save()
+        cov.html_report()
+
+
+setup(
+    name='pydist3d',
+    version='0.1',
+    description='Distance-based colocalisation analysis module',
+    author='Jeremy Metz',
+    author_email='j.metz@exeter.ac.uk',
+    packages=['pydist3d'],
+    test_suite="pydist3d.tests",
+    setup_requires=['pytest-runner'],
+    tests_require=['pytest'],
+    cmdclass={
+        "coverage": CoverageCommand,
+    },
+)
