6efa54fa74bab6a0efe1423cb4e6a317e90fb15f
Jeremy Metz <metz.jp@gmail.com>
2020-11-18 12:51:03 +0100

    Refactor of GUI, added scale section

--- pybioprox_gui.py
+++ pybioprox_gui.py
@@ -10,9 +10,260 @@ import os
 import sys
 from dataclasses import make_dataclass, asdict
 from PyQt5 import QtWidgets  # type: ignore
-from pybioprox import main as pybioprox_main
+from PyQt5.QtCore import pyqtSignal  # pylint: disable=no-name-in-module
 import matplotlib.pyplot as plt
+from pybioprox import main as pybioprox_main
 plt.switch_backend('qt5agg')
+
+
+class FolderSelectionWidget(QtWidgets.QWidget):
+    """
+    Label, Text Edit, and Selection button to select
+    a folder
+    """
+    # pylint: disable=c-extension-no-member
+    folder_selected = pyqtSignal(str)
+
+    def __init__(
+            self,
+            *args,
+            label_text="Folder",
+            button_text="Select",
+            dialog_caption="Select folder",
+            sizes=(1, 4, 1),
+            slot=None,
+            **kwargs):
+        super().__init__(*args, **kwargs)
+        self.sizes = sizes
+        self.starts = [
+            sum(self.sizes[:ind])
+            for ind in range(len(sizes))]
+        self.dialog_caption = dialog_caption
+        self.label = QtWidgets.QLabel(label_text, self)
+        self.button = QtWidgets.QPushButton(button_text, self)
+        self.line_edit = QtWidgets.QLineEdit(self)
+        self.line_edit.setReadOnly(True)
+        self.folder = None
+        self.button.clicked.connect(self.select_folder)
+        if slot:
+            self.folder_selected.connect(slot)
+        self.apply_layout()
+
+    def apply_layout(self):
+        """
+        Generate the layout of the child controls
+        """
+        layout = QtWidgets.QGridLayout()
+        layout.addWidget(self.label, 0, self.starts[0], 1, self.sizes[0])
+        layout.addWidget(self.line_edit, 0, self.starts[1], 1, self.sizes[1])
+        layout.addWidget(self.button, 0, self.starts[2], 1, self.sizes[2])
+        layout.setContentsMargins(0, 0, 0, 0)
+        self.setLayout(layout)
+
+    def select_folder(self):
+        """
+        Select a folder and set the folder attribute
+        """
+        self.folder = QtWidgets.QFileDialog.getExistingDirectory(
+            parent=self,
+            caption=self.dialog_caption,
+        )
+        self.line_edit.setText(self.folder)
+        self.folder_selected.emit(self.folder)
+
+
+class ScalesWidget(QtWidgets.QWidget):
+    """
+    Label, and QDoubleSpinBox to select each of the 3 dimension
+    sizes and labels and line-edits to select units names
+    """
+    # pylint: disable=c-extension-no-member
+
+    def __init__(
+            self,
+            *args,
+            sizes=(1, 1, 1, 1),
+            # dimensions=("x", "y", "z"),
+            dimensions=("x-y", "z"),
+            **kwargs):
+        super().__init__(*args, **kwargs)
+        self.sizes = sizes
+        self.labels_scale = [
+            QtWidgets.QLabel(f"Scale {dim}", self)
+            for dim in dimensions]
+        self.entry_scales = [
+            QtWidgets.QDoubleSpinBox(self, value=1)
+            for dim in dimensions]
+        self.labels_unit_name = [
+            QtWidgets.QLabel(f"Units {dim}", self)
+            for dim in dimensions]
+        self.entry_units = [
+            QtWidgets.QLineEdit(self, text="pixels")
+            for dim in dimensions]
+
+        self.apply_layout()
+
+    def __len__(self):
+        """
+        Get the length of the widget, by summing the individual
+        element sizes (in grid coordinates)
+        """
+        return sum(self.sizes)
+
+    def apply_layout(self):
+        """
+        Generate the layout of the child controls
+        """
+        starts = [
+            sum(self.sizes[:ind])
+            for ind in range(len(self.sizes))]
+        layout = QtWidgets.QGridLayout()
+        for start, size, label in zip(
+                starts[::2], self.sizes[::2], self.labels_scale):
+            layout.addWidget(label, 0, start, 1, size)
+        for start, size, control in zip(
+                starts[1::2], self.sizes[1::2], self.entry_scales):
+            layout.addWidget(control, 0, start, 1, size)
+        for start, size, label in zip(
+                starts[::2], self.sizes[::2], self.labels_unit_name):
+            layout.addWidget(label, 1, start, 1, size)
+        for start, size, control in zip(
+                starts[1::2], self.sizes[1::2], self.entry_units):
+            layout.addWidget(control, 1, start, 1, size)
+
+        layout.setContentsMargins(0, 0, 0, 0)
+        self.setLayout(layout)
+
+
+class ChannelSelectionWidget(QtWidgets.QWidget):
+    """
+    Label, and combo boxes, to select channels
+    """
+    # pylint: disable=c-extension-no-member
+    # channels_changed = pyqtSignal(list)
+
+    def __init__(
+            self,
+            *args,
+            sizes=(1, 1, 1, 1),
+            label1="Measure from channel",
+            label2="to channel",
+            channel_1_options=("1", "2", "3"),
+            channel_2_options=("1", "2", "3"),
+            # slot=None,
+            **kwargs):
+        super().__init__(*args, **kwargs)
+        self.sizes = sizes
+        self.label1 = QtWidgets.QLabel(label1, self)
+        self.label2 = QtWidgets.QLabel(label2, self)
+        self.select_channel_1 = QtWidgets.QComboBox(self)
+        self.select_channel_1.addItems(channel_1_options)
+        self.select_channel_2 = QtWidgets.QComboBox(self)
+        self.select_channel_2.addItems(channel_2_options)
+        self.select_channel_2.setCurrentIndex(1)
+        self.channels = [0, 1]
+        self.select_channel_1.currentIndexChanged.connect(
+            self.change_channel_1)
+        self.select_channel_2.currentIndexChanged.connect(
+            self.change_channel_2)
+        # if slot:
+        #     self.folder_selected.connect(slot)
+        self.apply_layout()
+
+    def apply_layout(self):
+        """
+        Generate the layout of the child controls
+        """
+        starts = [
+            sum(self.sizes[:ind])
+            for ind in range(len(self.sizes))]
+        layout = QtWidgets.QGridLayout()
+        layout.addWidget(self.label1, 0, starts[0], 1, self.sizes[0])
+        layout.addWidget(self.select_channel_1, 0, starts[1], 1, self.sizes[1])
+        layout.addWidget(self.label2, 0, starts[2], 1, self.sizes[2])
+        layout.addWidget(self.select_channel_2, 0, starts[3], 1, self.sizes[3])
+        layout.setContentsMargins(0, 0, 0, 0)
+        self.setLayout(layout)
+
+    def change_channel_1(self, value):
+        """
+        Slot to handle channel 1 selection
+        """
+        self.channels[0] = value
+
+    def change_channel_2(self, value):
+        """
+        Slot to handle channel 2 selection
+        """
+        self.channels[1] = value
+
+
+class FilterThresholdSelectionWidget(QtWidgets.QWidget):
+    """
+    Label, and combo boxes, to select filtering
+    and thresholding settings
+    """
+    # pylint: disable=c-extension-no-member
+    # channels_changed = pyqtSignal(list)
+
+    def __init__(
+            self,
+            *args,
+            sizes=(1, 1, 1, 1),
+            label_filtering="Filtering",
+            label_thresholding="Thresholding",
+            threshold_options=("None", "Otsu", "Li"),
+            filter_options=("None", "Gaussian (sigma 3px)"),
+            # slot=None,
+            **kwargs):
+        super().__init__(*args, **kwargs)
+        self.sizes = sizes
+        self.label_filtering = QtWidgets.QLabel(label_filtering, self)
+        self.label_thresholding = QtWidgets.QLabel(label_thresholding, self)
+        self.select_filter = QtWidgets.QComboBox(self)
+        self.select_filter.addItems(filter_options)
+        self.select_threshold = QtWidgets.QComboBox(self)
+        self.select_threshold.addItems(threshold_options)
+        self.filter = None
+        self.threshold = None
+        self.select_filter.currentTextChanged.connect(
+            self.change_filter)
+        self.select_threshold.currentTextChanged.connect(
+            self.change_threshold)
+        # if slot:
+        #     self.folder_selected.connect(slot)
+        self.apply_layout()
+
+    def apply_layout(self):
+        """
+        Generate the layout of the child controls
+        """
+        layout = QtWidgets.QGridLayout()
+        starts = [
+            sum(self.sizes[:ind])
+            for ind in range(len(self.sizes))]
+        layout.addWidget(
+            self.label_filtering, 0, starts[0], 1, self.sizes[0])
+        layout.addWidget(
+            self.select_filter, 0, starts[1], 1, self.sizes[1])
+        layout.addWidget(
+            self.label_thresholding, 0, starts[2], 1, self.sizes[2])
+        layout.addWidget(
+            self.select_threshold, 0, starts[3], 1, self.sizes[3])
+        layout.setContentsMargins(0, 0, 0, 0)
+        self.setLayout(layout)
+
+    def change_filter(self, value):
+        """
+        Slot to handle filter selection
+        """
+        self.filter = value
+
+    def change_threshold(self, value):
+        """
+        Slot to handle threshold selection
+        """
+        self.threshold = value
 
 
 class PyBioProxSettingsWidget(QtWidgets.QWidget):
@@ -25,12 +276,11 @@ class PyBioProxSettingsWidget(QtWidgets.QWidget):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         # self.resize(600, 480)
-        self.select_input_text = None
-        self.select_output_text = None
-        self.select_filter_select = None
-        self.select_threshold_select = None
-        self.select_channel_1 = None
-        self.select_channel_2 = None
+        self.select_input_widget = None
+        self.select_output_widget = None
+        self.select_channels_widget = None
+        self.select_filter_threshold_widget = None
+        self.select_scale_widget = None
         self.run_button = None
         self.generate_controls()
         self.generate_layout()
@@ -41,23 +291,20 @@ class PyBioProxSettingsWidget(QtWidgets.QWidget):
         """
         Adds controls
         """
-        threshold_options = ["None", "Otsu", "Li"]
-        filter_options = ["None", "Gaussian (sigma 3px)"]
-        channel_1_options = ["1", "2", "3"]
-        channel_2_options = ["1", "2", "3"]
-        self.select_input_text = QtWidgets.QLineEdit(self)
-        self.select_input_text.setReadOnly(True)
-        self.select_output_text = QtWidgets.QLineEdit(self)
-        self.select_output_text.setReadOnly(True)
-        self.select_filter_select = QtWidgets.QComboBox(self)
-        self.select_filter_select.addItems(filter_options)
-        self.select_threshold_select = QtWidgets.QComboBox(self)
-        self.select_threshold_select.addItems(threshold_options)
-        self.select_channel_1 = QtWidgets.QComboBox(self)
-        self.select_channel_1.addItems(channel_1_options)
-        self.select_channel_2 = QtWidgets.QComboBox(self)
-        self.select_channel_2.addItems(channel_2_options)
-        self.select_channel_2.setCurrentIndex(1)
+        self.select_input_widget = FolderSelectionWidget(
+            self,
+            label_text="Input folder",
+            dialog_caption="Select input folder",
+            slot=self.input_folder_changed)
+        self.select_output_widget = FolderSelectionWidget(
+            self,
+            label_text="Output folder",
+            dialog_caption="Select output folder",
+            slot=self.output_folder_changed)
+        self.select_channels_widget = ChannelSelectionWidget(self)
+        self.select_filter_threshold_widget = FilterThresholdSelectionWidget(
+            self)
+        self.select_scale_widget = ScalesWidget(self)
         self.preview_button = QtWidgets.QPushButton("Preview", self)
         self.preview_button.setEnabled(False)
         self.preview_button.clicked.connect(self.run_preview)
@@ -69,79 +316,42 @@ class PyBioProxSettingsWidget(QtWidgets.QWidget):
         """
         Generates the layout
         """
-        select_input_label = QtWidgets.QLabel("Input folder", self)
-        select_input_button = QtWidgets.QPushButton("Select", self)
-        select_input_button.clicked.connect(self.select_input)
-
-        select_output_label = QtWidgets.QLabel("Output folder", self)
-        select_output_button = QtWidgets.QPushButton("Select", self)
-        select_output_button.clicked.connect(self.select_output)
-
-        select_filter_label = QtWidgets.QLabel("Filtering", self)
-
-        select_threshold_label = QtWidgets.QLabel("Thresholding", self)
-
-        select_channel_1_label = QtWidgets.QLabel("Measure from channel", self)
-
-        select_channel_2_label = QtWidgets.QLabel("to channel", self)
 
         cancel_button = QtWidgets.QPushButton("Cancel", self)
         cancel_button.clicked.connect(
             lambda: QtWidgets.QApplication.instance().exit(-1))
 
         layout = QtWidgets.QGridLayout()
-        layout.addWidget(select_input_label, 0, 0)
-        layout.addWidget(self.select_input_text, 0, 1, 1, 3)
-        layout.addWidget(select_input_button, 0, 4)
+        layout.addWidget(self.select_input_widget, 0, 0, 1, 6)
+        layout.addWidget(self.select_output_widget, 1, 0, 1, 6)
+        layout.addWidget(self.select_channels_widget, 2, 0, 1, 6)
+        layout.addWidget(self.select_filter_threshold_widget, 3, 0, 1, 6)
+        len_scale_widget = len(self.select_scale_widget)
+        layout.addWidget(
+            self.select_scale_widget,
+            4, 6-len_scale_widget, 1, len_scale_widget)
 
-        layout.addWidget(select_output_label, 1, 0)
-        layout.addWidget(self.select_output_text, 1, 1, 1, 3)
-        layout.addWidget(select_output_button, 1, 4)
-
-        layout.addWidget(select_channel_1_label, 2, 0)
-        layout.addWidget(self.select_channel_1, 2, 1)
-
-        layout.addWidget(select_channel_2_label, 2, 2)
-        layout.addWidget(self.select_channel_2, 2, 3)
-
-        layout.addWidget(select_filter_label, 3, 1)
-        layout.addWidget(self.select_filter_select, 3, 2, 1, 2)
-
-        layout.addWidget(select_threshold_label, 4, 1)
-        layout.addWidget(self.select_threshold_select, 4, 2, 1, 2)
-
-        layout.addWidget(cancel_button, 5, 2)
-        layout.addWidget(self.preview_button, 5, 3)
-        layout.addWidget(self.run_button, 5, 4)
+        layout.addWidget(cancel_button, 5, 3)
+        layout.addWidget(self.preview_button, 5, 4)
+        layout.addWidget(self.run_button, 5, 5)
         self.setLayout(layout)
 
-    def select_input(self):
+    def input_folder_changed(self, input_folder):
         """
         Select the input folder using a standard Qt Dialog.
         Also then conditionally sets the run button as enabled
         """
-        input_folder = QtWidgets.QFileDialog.getExistingDirectory(
-            parent=self,
-            caption="Select input data folder",
-        )
-        output_folder = self.select_output_text.text()
-        self.select_input_text.setText(input_folder)
-
+        output_folder = self.select_output_widget.folder
         can_run = bool(input_folder and output_folder)
         self.run_button.setEnabled(can_run)
         self.preview_button.setEnabled(bool(input_folder))
 
-    def select_output(self):
+    def output_folder_changed(self, output_folder):
         """
         Select the output folder using a standard Qt Dialog.
         Also then conditionally sets the run button as enabled
         """
-        input_folder = self.select_input_text.text()
-        output_folder = QtWidgets.QFileDialog.getExistingDirectory(
-            parent=self,
-            caption="Select output folder",
-        )
-        self.select_output_text.setText(output_folder)
+        input_folder = self.select_input_widget.folder
         can_run = bool(input_folder and output_folder)
         self.run_button.setEnabled(can_run)
 
@@ -150,12 +360,16 @@ class PyBioProxSettingsWidget(QtWidgets.QWidget):
         Return the settings as a dictionary
         """
         settings = {
-            "input_folder": self.select_input_text.text(),
-            "output_folder": self.select_output_text.text(),
-            "channel1_index": int(self.select_channel_1.currentText())-1,
-            "channel2_index": int(self.select_channel_2.currentText())-1,
-            "filter_method": self.select_filter_select.currentText(),
-            "threshold_method": self.select_threshold_select.currentText(),
+            "input_folder": self.select_input_widget.folder,
+            "output_folder": self.select_output_widget.folder,
+            "channel1_index": self.select_channels_widget.channels[0],
+            "channel2_index": self.select_channels_widget.channels[1],
+            "filter_method": self.select_filter_threshold_widget.filter,
+            "threshold_method": self.select_filter_threshold_widget.threshold,
+            "scales": [control.value()
+                       for control in self.select_scale_widget.entry_scales],
+            "units": [control.text()
+                      for control in self.select_scale_widget.entry_units],
         }
         return make_dataclass(
             "Settings", settings.keys(), frozen=True)(**settings)
@@ -217,19 +431,17 @@ class PyBioProxSettingsWidget(QtWidgets.QWidget):
 
         channel1, channel2 = pybioprox_main.load_data(
             filepath, config.channel1_index, config.channel2_index)
-        mask1, mask2 = pybioprox_main.detect_objects(
-            channel1, channel2, config)
+        try:
+            mask1, mask2 = pybioprox_main.detect_objects(
+                channel1, channel2, config)
 
-        pybioprox_main.plot_outlines(
-            channel1, channel2, mask1, mask2)
-        plt.show(block=False)
-        # plt.figure()
-        # plt.subplot(1, 2, 1)
-        # plt.imshow(channel1, cmap='gray')
-        # plt.imshow(mask
-        # plt.subplot(1, 2, 2)
-        # plt.imshow(1, 2, 2)
-        # plt.show()
+            pybioprox_main.plot_outlines(
+                channel1, channel2, mask1, mask2)
+            plt.show(block=False)
+        except Exception as err:  # pylint: disable=broad-except
+            self.show_problem(
+                "An error occurred while previewing",
+                str(err))
 
 
 def get_settings():
