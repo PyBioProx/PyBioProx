3f5534fed37209d73eb585f9938dde95141fe2cd
Jeremy Metz <metz.jp@gmail.com>
2020-11-27 15:24:40 +0100

    Moved dependencies to setup.py to improve install
    * Means pip install will now install dependencies instead of having to
      manually run pip install -r requirements.txt
    * Added more robustness to setup.py re coverage - now
      an end user can install the module without requiring coverage or
      pytest

--- setup.py
+++ setup.py
@@ -6,14 +6,26 @@ Setup script for pybioprox module
 J. Metz <metz.jp@gmail.com>
 """
 import os
-import unittest
 from typing import List
+import importlib
 from setuptools import setup, Command
-# Coverage command
-import coverage
-import pytest
+
 
 MODNAME = "pybioprox"
+
+
+def create_cmdclass():
+    """
+    Adds coverage command class if coverage and pytest are available
+    """
+    try:
+        importlib.import_module("coverage")
+        importlib.import_module("pytest")
+        return {
+            "coverage": CoverageCommand
+        }
+    except ImportError:
+        return {}
 
 
 class CoverageCommand(Command):
@@ -31,6 +43,7 @@ class CoverageCommand(Command):
 
     def run(self):  # pylint: disable=no-self-use
         """runner"""
+        # Coverage command
         # NOTE: Use API as follows for better control
         omitfiles = [
             os.path.join(MODNAME, "tests", "unit", "*"),
@@ -41,6 +54,8 @@ class CoverageCommand(Command):
                 os.path.join(root, fname) for fname in
                 filter(lambda f: f == "__init__.py", files)
             )
+        coverage = importlib.import_module("coverage")
+        pytest = importlib.import_module("pytest")
 
         print("Running coverage on", MODNAME)
         cov = coverage.Coverage(
@@ -67,9 +82,18 @@ setup(
     packages=[MODNAME],
     test_suite=f"{MODNAME}.tests",
     setup_requires=['pytest-runner'],
+    install_requires=[
+        "imageio==2.6.1",
+        "scipy==1.4.1",
+        "scikit_image==0.16.2",
+        "colorama==0.4.3",
+        "coloredlogs==14.0",
+        "pandas==1.0.3",
+        "numpy>=1.18",
+        "tifffile==2020.2.16",
+        "matplotlib==3.2.1",
+    ],
     tests_require=['pytest'],
     scripts=['pybioprox_gui.py'],
-    cmdclass={
-        "coverage": CoverageCommand,
-    },
+    cmdclass=create_cmdclass(),
 )
